d4ac2ccd30294c45ff23b54146bce8ce016edbbb
zzzyoonnn <dwsrh03@gmail.com>
2024-06-11 10:55:46 +0900

    Section4. 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,7 +3,6 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -1,9 +1,12 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
