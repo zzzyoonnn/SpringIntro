d42a65bba7fdb2030cb7943e6b858fc568bee9ea
zzzyoonnn <dwsrh03@gmail.com>
2024-06-12 13:04:10 +0900

    Section5. 회원 웹 기능 - 등록

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,12 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -23,5 +27,21 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // 회원 웹 기능 - 등록
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "rediredct:/";
     }
 }
---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hello_spring.controller;
+
+public class MemberForm {
+
+    // createMemberForm의 name과 매칭
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
