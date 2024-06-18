f25833790adca572d41b46e735ef3770b2458ef3
zzzyoonnn <dwsrh03@gmail.com>
2024-06-18 22:00:46 +0900

    Section6. 스프링 데이터 JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,9 +1,7 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +9,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired  // 생성자가 하나인 경우 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean       // memberService을 스프링 빈에 등록
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+
+    /*
     @Bean       // memberRepository를 스프링 빈에 등록
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
@@ -30,4 +30,5 @@ public class SpringConfig {
         //return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
     }
+     */
 }
